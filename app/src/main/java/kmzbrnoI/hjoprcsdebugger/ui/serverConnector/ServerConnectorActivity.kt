c3e0eab0c8ab79ecda17f11bd9dd64b979978f56
HanaNavratilova <hanca.navratilova@volny.cz>
2020-05-04 23:56:11 +0200

    add back button to server connector

--- app/src/main/java/kmzbrnoI/hjoprcsdebugger/ui/serverConnector/ServerConnectorActivity.kt
+++ app/src/main/java/kmzbrnoI/hjoprcsdebugger/ui/serverConnector/ServerConnectorActivity.kt
@@ -3,11 +3,14 @@ package kmzbrnoI.hjoprcsdebugger.ui.serverConnector
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kmzbrnoI.hjoprcsdebugger.R
+import kmzbrnoI.hjoprcsdebugger.network.TCPClientApplication
 
 class ServerConnectorActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.server_connector_activity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if (savedInstanceState == null) {
             val fragment = ServerConnector()
@@ -15,5 +18,11 @@ class ServerConnectorActivity: AppCompatActivity() {
                 .replace(R.id.server_connector_activity, fragment)
                 .commit()
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        TCPClientApplication.getInstance().disconnect()
+        onBackPressed()
+        return true
     }
 }
