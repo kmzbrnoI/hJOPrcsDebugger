a3c1fde5b6e846cd5bd0d93845419a749b919b3e
Jan Horacek <me@apophis.cz>
2020-06-06 12:13:31 +0200

    S-COM alert: allow to cancel.

--- app/src/main/java/kmzbrnoI/hjoprcsdebugger/ui/moduleInfo/ModuleAdapter.kt
+++ app/src/main/java/kmzbrnoI/hjoprcsdebugger/ui/moduleInfo/ModuleAdapter.kt
@@ -116,7 +116,7 @@ class ModuleAdapter(
 
                     dialog.dismiss()
                 }
-                .setCancelable(false)
+                .setCancelable(true)
                 .show()
         }
     }
